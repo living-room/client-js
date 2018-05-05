b4eabba9c4c68d8965add5b1cd4df8ca81d9a0f7
Jonathan Dahan <hi@jonathan.is>
2018-05-05 13:51:24 -0400

    fix examples/test.ks

--- examples/test.js
+++ examples/test.js
@@ -2,4 +2,7 @@ const Room = require('../build/room.js')
 const room = new Room()
 
 room.select('$a is a $b animal at ($c, $d)').then(console.dir)
-room.subscribe([`$a is a $aspecies animal at ($ax, $ay)`], console.dir)
+room.subscribe([`$a is a $aspecies animal at ($ax, $ay)`], ({assertions}) => {
+  assertions.forEach(console.dir)
+})
+setTimeout(() => room.assert('bleep is a test animal at (0, 0)'), 500)
