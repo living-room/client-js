5469800f3d85ae53e929e23611a0981a7a783b1c
Peter Coles <peter@mrcoles.com>
2018-05-15 17:53:31 -0400

    prettier

--- src/room.js
+++ src/room.js
@@ -1,41 +1,41 @@
-'use strict'
+'use strict';
 
 /**
  * Creates a new http client to a roomdb instance
  *
  * @param {host} host of living room server (defaults to http://localhost:3000)
  */
-import fetch from 'node-fetch'
-import io from 'socket.io-client'
-import bonjour from 'nbonjour'
+import fetch from 'node-fetch';
+import io from 'socket.io-client';
+import bonjour from 'nbonjour';
 
-function getEnv (key) {
-  if (typeof process !== 'undefined') return process.env[key]
+function getEnv(key) {
+  if (typeof process !== 'undefined') return process.env[key];
 }
 
 export default class Room {
-  constructor (host) {
-    this._host = host || getEnv('LIVING_ROOM_HOST') || 'http://localhost:3000'
-    if (!this._host.startsWith('http://')) this._host = `http://${this._host}`
-    const serviceDefinition = { type: 'http', subtypes: ['livingroom'] }
+  constructor(host) {
+    this._host = host || getEnv('LIVING_ROOM_HOST') || 'http://localhost:3000';
+    if (!this._host.startsWith('http://')) this._host = `http://${this._host}`;
+    const serviceDefinition = { type: 'http', subtypes: ['livingroom'] };
 
     if (false) {
       this._browser = bonjour.create().find(serviceDefinition, service => {
-        const { type, host, port } = service
-        this._host = `${type}://${host}:${port}`
-        console.log(`set new host to ${this._host}`)
-        this.connect()
-      })
+        const { type, host, port } = service;
+        this._host = `${type}://${host}:${port}`;
+        console.log(`set new host to ${this._host}`);
+        this.connect();
+      });
     }
-    this.connect()
+    this.connect();
   }
 
-  connect () {
-    this._socket = io.connect(this._host)
+  connect() {
+    this._socket = io.connect(this._host);
     if (typeof window === 'object') {
       this._socket.on('reconnect', () => {
-        window.location.reload(true)
-      })
+        window.location.reload(true);
+      });
     }
   }
 
@@ -43,11 +43,11 @@ export default class Room {
    * @param {String | String[]} facts
    * @param {Function} callback
    */
-  subscribe (facts, callback) {
-    if (typeof facts === 'string') facts = [facts]
-    const patternsString = JSON.stringify(facts)
-    this._socket.on(patternsString, callback)
-    this._socket.emit('subscribe', patternsString)
+  subscribe(facts, callback) {
+    if (typeof facts === 'string') facts = [facts];
+    const patternsString = JSON.stringify(facts);
+    this._socket.on(patternsString, callback);
+    this._socket.emit('subscribe', patternsString);
   }
 
   /**
@@ -55,58 +55,60 @@ export default class Room {
    * @param {String} endpoint assert, retract, select
    * @param {[String]} facts
    */
-  _request (endpoint, facts, callback) {
+  _request(endpoint, facts, callback) {
     if (!['assert', 'retract', 'select', 'facts'].includes(endpoint)) {
-      throw new Error('Unknown endpoint, try assert, retract, select, or facts')
+      throw new Error(
+        'Unknown endpoint, try assert, retract, select, or facts'
+      );
     }
 
-    if (typeof facts === 'string') facts = [facts]
+    if (typeof facts === 'string') facts = [facts];
 
     if (!(endpoint === 'facts' || facts.length)) {
-      throw new Error('Please pass at least one fact')
+      throw new Error('Please pass at least one fact');
     }
 
     // Can this return a promise with the result?
     // Does that even make sense?
     if (this._socket.connected) {
       return new Promise((resolve, reject) => {
-        this._socket.emit(endpoint, facts, resolve)
-      })
+        this._socket.emit(endpoint, facts, resolve);
+      });
     }
 
-    const uri = `${this._host}/${endpoint}`
+    const uri = `${this._host}/${endpoint}`;
 
     const post = {
       method: 'POST',
       body: JSON.stringify({ facts }),
       headers: { 'Content-Type': 'application/json' }
-    }
+    };
 
     return fetch(uri, post)
       .then(response => response.json())
       .catch(error => {
         if (error.code === 'ECONNREFUSED') {
-          console.error(`No server listening on ${uri}`)
-          console.error(`Try 'npm start' to run a local service`)
+          console.error(`No server listening on ${uri}`);
+          console.error(`Try 'npm start' to run a local service`);
         } else {
-          console.error(error)
+          console.error(error);
         }
-      })
+      });
   }
 
-  assert (facts, callback) {
-    return this._request('assert', facts, callback)
+  assert(facts, callback) {
+    return this._request('assert', facts, callback);
   }
 
-  retract (facts, callback) {
-    return this._request('retract', facts, callback)
+  retract(facts, callback) {
+    return this._request('retract', facts, callback);
   }
 
-  select (facts, callback) {
-    return this._request('select', facts, callback)
+  select(facts, callback) {
+    return this._request('select', facts, callback);
   }
 
-  facts () {
-    return this._request('facts')
+  facts() {
+    return this._request('facts');
   }
 }
