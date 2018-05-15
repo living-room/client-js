04979212d4271de2ba591d7759db4617aa6c7739
Peter Coles <peter@mrcoles.com>
2018-05-15 18:09:58 -0400

    ran build

--- build/room.js
+++ build/room.js
@@ -6,18 +6,18 @@ var fetch = _interopDefault(require('node-fetch'));
 var io = _interopDefault(require('socket.io-client'));
 require('nbonjour');
 
-function getEnv (key) {
-  if (typeof process !== 'undefined') return process.env[key]
+function getEnv(key) {
+  if (typeof process !== 'undefined') return process.env[key];
 }
 
 class Room {
-  constructor (host) {
+  constructor(host) {
     this._host = host || getEnv('LIVING_ROOM_HOST') || 'http://localhost:3000';
     if (!this._host.startsWith('http://')) this._host = `http://${this._host}`;
     this.connect();
   }
 
-  connect () {
+  connect() {
     this._socket = io.connect(this._host);
     if (typeof window === 'object') {
       this._socket.on('reconnect', () => {
@@ -30,7 +30,7 @@ class Room {
    * @param {String | String[]} facts
    * @param {Function} callback
    */
-  subscribe (facts, callback) {
+  subscribe(facts, callback) {
     if (typeof facts === 'string') facts = [facts];
     const patternsString = JSON.stringify(facts);
     this._socket.on(patternsString, callback);
@@ -42,15 +42,17 @@ class Room {
    * @param {String} endpoint assert, retract, select
    * @param {[String]} facts
    */
-  _request (endpoint, facts, callback) {
+  _request(endpoint, facts, callback) {
     if (!['assert', 'retract', 'select', 'facts'].includes(endpoint)) {
-      throw new Error('Unknown endpoint, try assert, retract, select, or facts')
+      throw new Error(
+        'Unknown endpoint, try assert, retract, select, or facts'
+      );
     }
 
     if (typeof facts === 'string') facts = [facts];
 
     if (!(endpoint === 'facts' || facts.length)) {
-      throw new Error('Please pass at least one fact')
+      throw new Error('Please pass at least one fact');
     }
 
     // Can this return a promise with the result?
@@ -58,7 +60,7 @@ class Room {
     if (this._socket.connected) {
       return new Promise((resolve, reject) => {
         this._socket.emit(endpoint, facts, resolve);
-      })
+      });
     }
 
     const uri = `${this._host}/${endpoint}`;
@@ -73,28 +75,31 @@ class Room {
       .then(response => response.json())
       .catch(error => {
         if (error.code === 'ECONNREFUSED') {
-          console.error(`No server listening on ${uri}`);
-          console.error(`Try 'npm start' to run a local service`);
+          let customError = new Error(
+            `No server listening on ${uri}. Try 'npm start' to run a local service.`
+          );
+          customError.code = 'NOTLISTENING';
+          throw customError;
         } else {
-          console.error(error);
+          throw error;
         }
-      })
+      });
   }
 
-  assert (facts, callback) {
-    return this._request('assert', facts, callback)
+  assert(facts, callback) {
+    return this._request('assert', facts, callback);
   }
 
-  retract (facts, callback) {
-    return this._request('retract', facts, callback)
+  retract(facts, callback) {
+    return this._request('retract', facts, callback);
   }
 
-  select (facts, callback) {
-    return this._request('select', facts, callback)
+  select(facts, callback) {
+    return this._request('select', facts, callback);
   }
 
-  facts () {
-    return this._request('facts')
+  facts() {
+    return this._request('facts');
   }
 }
 
