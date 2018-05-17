d9b69767153f1da4137ae35639ef2f035aed9c3d
Jonathan Dahan <hi@jonathan.is>
2018-05-17 11:02:29 -0400

    Enable bonjour to keep a local set of hosts
    you can cycle through hosts with room.nexthost()

--- src/room.js
+++ src/room.js
@@ -17,21 +17,34 @@ export default class Room {
   constructor (host) {
     this._host = host || getEnv('LIVING_ROOM_HOST') || 'http://localhost:3000'
     if (!this._host.startsWith('http://')) this._host = `http://${this._host}`
-    const serviceDefinition = { type: 'http', subtypes: ['livingroom'] }
+    this._hosts = new Set(this._host)
 
-    if (false) {
+    if (bonjour) {
+      const serviceDefinition = { type: 'http', subtypes: ['livingroom'] }
       this._browser = bonjour.create().find(serviceDefinition, service => {
         const { type, host, port } = service
-        this._host = `${type}://${host}:${port}`
-        console.log(`set new host to ${this._host}`)
-        this.connect()
+        const uri = `${type}://${host}:${port}`
+        if (this._hosts.has(uri)) return
+        this._hosts.add(uri)
+        console.log(`found new host ${uri}`)
+        console.log(`use \`room.nexthost()\` to cycle through hosts`)
       })
     }
     this.connect()
   }
 
+  nexthost () {
+    io.disconnect(this._host)
+    const ordered = Array.from(this._hosts.values()).sort()
+    const index = ordered.indexOf(this._host)
+    console.write(`switching from ${this._host}`)
+    this._host = ordered[(index + 1) % ordered.length]
+    this.connect()
+  }
+
   connect () {
     this._socket = io.connect(this._host)
+    console.log(`connecting to ${this._host}`)
     if (typeof window === 'object') {
       this._socket.on('reconnect', () => {
         window.location.reload(true)
